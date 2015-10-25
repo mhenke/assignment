4d6136dbf6919af794dc5712e33aa720ccdb733f
Mike Henke <henke.mike@gmail.com>
2015-10-25 23:22:45 +0000

    cleanup some code

--- modules/giphders/client/controllers/giphders.client.controller.js
+++ modules/giphders/client/controllers/giphders.client.controller.js
@@ -160,7 +160,7 @@ angular.module('giphders').controller('GiphdersController', ['$scope', '$statePa
     };
     
     $scope.copyAlert = function () {
-      var message = '<strong> Outstanding!</strong>  You copied your favorites to the clipboard.';
+      var message = '<strong> Outstanding!</strong>  You copied your favorite urls to the clipboard.';
       Flash.create('info', message, 'customAlert');
     };
     
